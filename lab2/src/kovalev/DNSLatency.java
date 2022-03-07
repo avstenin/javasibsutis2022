0db548002f2a4f7a4e4c3f8bce7f067796691b12
Belreau Cohen <nuu.chah.nulth@disroot.org>
2022-03-08 03:28:46 +0700

    Обработка исключения с прерыванием

--- lab2/src/kovalev/DNSLatency.java
+++ lab2/src/kovalev/DNSLatency.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class DNSLatency {
@@ -17,27 +18,27 @@ public class DNSLatency {
                 }
             }
 
-            for (int i = 1; i <= ipQueryNum; i++) {
-                System.out.println("Введите адрес сервера DNS" + String.valueOf(i) + ":");
-                String ip = scanner.next();
-                var query = PingTimeMeasure.findQuery(ip);
-                if (query != null) {
-                    System.out.println("Сервер по адресу '" + ip + "' уже был протестирован. Его результат: " + query.time() + " мс.");
-                    System.out.println("Хотите сбросить результат? [y/N]");
-                    char answer = (char) System.in.read();
-                    if ((answer == 'Y') || (answer == 'y'))
-                        PingTimeMeasure.remove(ip);
-                    else
-                        continue;
-                }
-                System.out.println();
-                try {
+            try {
+                for (int i = 1; i <= ipQueryNum; i++) {
+                    System.out.println("Введите адрес сервера DNS" + String.valueOf(i) + ":");
+                    String ip = scanner.next();
+                    var query = PingTimeMeasure.findQuery(ip);
+                    if (query != null) {
+                        System.out.println("Сервер по адресу '" + ip + "' уже был протестирован. Его результат: " + query.time() + " мс.");
+                        System.out.println("Хотите сбросить результат? [y/N]");
+                        char answer = (char) System.in.read();
+                        if ((answer == 'Y') || (answer == 'y'))
+                            PingTimeMeasure.remove(ip);
+                        else
+                            continue;
+                    }
+                    System.out.println();
                     PingTimeMeasure.add(ip);
-                } catch (InterruptedException e) {
-                    System.out.println("Зафиксировали прерывание, выходим.");
-                    break;
                 }
+            } catch (InterruptedException | NoSuchElementException e) {
+                System.out.println("Зафиксировали прерывание, выходим.");
             }
+            
         }
 
         PingTimeMeasure.print();
