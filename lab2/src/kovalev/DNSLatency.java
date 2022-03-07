0abe5362f77571551c9a8490819e6be4b03e6460
Belreau Cohen <nuu.chah.nulth@disroot.org>
2022-03-08 01:33:47 +0700

    Добавлена возможность ввести число серверов

--- lab2/src/kovalev/DNSLatency.java
+++ lab2/src/kovalev/DNSLatency.java
@@ -2,10 +2,21 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class DNSLatency {
-    final private static int ipQueryNum = 3;
     public static void main(String[] args)
     throws IOException {
         try (Scanner scanner = new Scanner(System.in)) {
+            int ipQueryNum;
+            System.out.println("Введите число серверов DNS:");
+            while (true) {
+                try {
+                    ipQueryNum = Integer.parseInt(scanner.next());
+                    break;
+                }
+                catch (NumberFormatException e) {
+                    System.out.println("Введите корректное число!");
+                }
+            }
+            
             for (int i = 1; i <= ipQueryNum; i++) {
                 System.out.println("Введите адрес сервера DNS" + String.valueOf(i) + ":");
                 String ip = scanner.next();
