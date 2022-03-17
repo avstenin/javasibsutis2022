17e50b9189620ab05f7c08b05153f6bcb15c2fec
Menchshikov Danil <freeestyler231@gmail.com>
2022-03-17 22:29:48 +0700

    Implemented input into array

--- lab2/src/menchshikov/DNSLab2.java
+++ lab2/src/menchshikov/DNSLab2.java
@@ -1,5 +1,34 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.time.*;
+import java.util.*;
+
 public class DNSLab2 {
+
+    public static Duration SpeedTest(String host) {
+        Instant startTime = Instant.now();
+        try {
+            InetAddress address = InetAddress.getByName(host);
+            if (address.isReachable(1000)) {
+                return Duration.between(startTime, Instant.now());
+            }
+        } catch (IOException e) {
+            //
+        }
+        return Duration.ofDays(1);
+    }
+
     public static void main(String[] args) {
-        System.out.println("");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input");
+        int inCount = in.nextInt();
+
+        ArrayList<String> addressArray = new ArrayList<>();
+        System.out.println("Input");
+        for (int i = 0; i <= inCount; i++) {
+            addressArray.add(in.nextLine());
+        }
+
+        in.close();
     }
 }
