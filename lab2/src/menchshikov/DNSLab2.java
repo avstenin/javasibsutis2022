b904b68d99c850dcb09cedb81bb132fa1609b063
Menchshikov Danil <freeestyler231@gmail.com>
2022-03-18 13:57:16 +0700

    Added output in file method

--- lab2/src/menchshikov/DNSLab2.java
+++ lab2/src/menchshikov/DNSLab2.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.time.*;
 import java.util.*;
@@ -41,17 +41,18 @@ public class DNSLab2 {
         return Duration.ofDays(1);
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input quantity of DNS address");
-        int inCount = sc.nextInt();
-
-        ArrayList<String> addressArray = new ArrayList<>();
-        System.out.println("Input DNS address");
-        for (int i = 0; i <= inCount; i++) {
-            addressArray.add(sc.nextLine());
+    public static void printInFile(ArrayList<DNS> DNS) throws IOException {
+        if (!DNS.isEmpty()) {
+            File fileOut = new File("out.txt");
+            FileWriter fw = new FileWriter(fileOut);
+            for (DNS out : DNS) {
+                fw.write(out.getIP() + ": " + out.getTime().toMillis() + "ms");
+            }
+            fw.close();
         }
+    }
 
+    public static void arrayInputOutpun(ArrayList<String> addressArray) throws IOException {
         ArrayList<DNS> DNSList = new ArrayList<>();
         for (String address : addressArray) {
             Duration time = Duration.ZERO;
@@ -68,8 +69,24 @@ public class DNSLab2 {
 
         System.out.println("Output:");
         for (DNS out : DNSList) {
-            System.out.println("\t" + out.getIP() + " " + out.getTime().toMillis() + "ms");
+            System.out.println("\t" + out.getIP() + ": " + out.getTime().toMillis() + "ms");
         }
+
+        printInFile(DNSList);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input quantity of DNS address");
+        int inCount = sc.nextInt();
+
+        ArrayList<String> addressArray = new ArrayList<>();
+        System.out.println("Input DNS address");
+        for (int i = 0; i <= inCount; i++) {
+            addressArray.add(sc.nextLine());
+        }
+
+        arrayInputOutpun(addressArray);
         sc.close();
     }
 }
