ddaf8b415846bde0ad6edd94f92d164b5bf53a65
rumeone <yuaroslav_29@mail.ru>
2022-03-21 20:17:21 +0700

    added part 3

--- lab2/src/Zakharov/lab2.java
+++ lab2/src/Zakharov/lab2.java
@@ -1,8 +1,16 @@
 package lab2.src.Zakharov;
 
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Date;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Map;
 
 public class lab2 {
     public static void main(String[] args) throws Exception {
@@ -19,8 +27,9 @@ public class lab2 {
             averageTime.add(getAverageTime(ip.get(i)));
             ipAndAverageTime.put(ip.get(i), Integer.parseInt(averageTime.get(i)));
         }
-        sortAverageTime(ipAndAverageTime);
+        //sortAverageTime(ipAndAverageTime);
         writeToFile(ipAndAverageTime);
+        printResult();
     }
 
     public static String getAverageTime(String ip) throws IOException {
@@ -46,17 +55,42 @@ public class lab2 {
                 .forEach(x -> System.out.println(x + "ms"));
     }
 
-    public static void writeToFile(HashMap averageTime) {
+    public static void writeToFile(HashMap<String, Integer> averageTime) {
         try {
             File file = new File("newfile.txt");
-            PrintWriter pw = new PrintWriter(file);
-
-            pw.close();
             if (!file.exists())
                 file.createNewFile();
+            FileWriter fw = new FileWriter(file, true);
+            averageTime.entrySet().stream()
+                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                    .forEach(x -> {
+                        try {
+                            fw.write(x + "ms\n");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+            fw.write("---" + new Date() + "\n");
+            fw.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Exception occur emsg= "+ e.getMessage());
+        }
+    }
+
+    public static void printResult() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("newfile.txt"));
+            String line;
+            while((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            br.close();
         } catch (IOException e) {
             System.out.println("Error " + e);
         }
     }
+
 }
 
