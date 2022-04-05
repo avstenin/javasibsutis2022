dc4b9436f8c317502f580a5a531c341e96d5a5d2
Ismailov <ferein01@gmail.com>
2022-04-05 19:25:47 +0700

    Deleted other projects

--- lab0/src/Zakharov/lab0.java
+++
@@ -1,7 +0,0 @@
-package lab0.src.Zakharov;
-
-public class lab0 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- lab0/src/fadin/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package lab0.src.fadin;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello W0rld");
-    }
-}
