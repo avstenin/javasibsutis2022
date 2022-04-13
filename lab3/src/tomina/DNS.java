e6145d0da46de492c7fc5dee124503c0a14f5070
Tomina A <tominaanastasiya2001@gmail.com>
2022-04-14 01:58:57 +0700

    Implemented the functionality of transferring a list by entering a file name

--- lab3/src/tomina/DNS.java
+++ lab3/src/tomina/DNS.java
@@ -1,2 +1,61 @@
-package tomina;public class DNS {
+package tomina;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+
+public class DNS {
+    private int time;
+    private String name;
+    private String ip_addr;
+
+    public int getTime() {
+        return time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIp_addr() {
+        return ip_addr;
+    }
+
+    public DNS(String ip_addr) {
+        this.ip_addr = ip_addr;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public static final Comparator<DNS> COMPARE_BY_TIME = new Comparator<DNS>() {
+        @Override
+        public int compare(DNS lhs, DNS rhs) {
+            return lhs.getTime() - rhs.getTime();
+        }
+    };
+
+    public static void write(ArrayList<DNS> arrDNS) throws IOException {
+        String date = new SimpleDateFormat("yyyy_MM_dd(HH;mm)").format(new Date(System.currentTimeMillis()));
+        File path = new File(date);
+        path.mkdir();
+        for (DNS dns : arrDNS) {
+            FileWriter writer = new FileWriter(path + "\\DNS" + arrDNS.indexOf(dns) + ".txt");
+            if (dns.time != -1) {
+                String text = "IP адрес DNS-сервера:" + dns.ip_addr + "\tDNS-сервер: " + dns.name + " \tСреднее время отклика: " + dns.time + " ms\n";
+                writer.write(text);
+            }
+            writer.flush();
+            writer.close();
+        }
+    }
 }
--- lab3/src/tomina/Main.java
+++ lab3/src/tomina/Main.java
@@ -1,6 +1,7 @@
 package tomina;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
@@ -16,43 +17,91 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int index = 0;
         int n = 0;
-        System.out.println("Введите количество DNS-серверов");
-        n = input.nextInt();
-        for (int i = 0; i < n; i++){
-            System.out.println("Введите адрес сервера DNS" + i +":");
-            arrDNS.add(new DNS(input.next()));
-            InetAddress inetAddress = java.net.InetAddress.getByName(arrDNS.get(i).getIp_addr());
-            arrDNS.get(i).setName(inetAddress.getHostName());
-            ProcessBuilder builder = new ProcessBuilder(
-                    "cmd.exe", "/c", "ping", arrDNS.get(i).getIp_addr());
-            builder.redirectErrorStream(true);
-            Process p = builder.start();
-            BufferedReader r = new BufferedReader(new
-                    InputStreamReader(p.getInputStream(),"866"));
-            String line;
-            String fullline = "";
-            while (true) {
-                line = r.readLine();
-                fullline=fullline+line;
-                if (line == null) { break; }
-            }
-            index = fullline.indexOf("Среднее");
-            if(index<0){
-                System.out.println("DNS не доступен");
-                arrDNS.get(i).setTime(-1);
-                continue;
-            }
-            String time = fullline.substring(index+10, index+14);
-            time = time.replaceAll("[a-zA-Zа-яА-Я]*", "");
-            time = time.replaceAll(" ", "");
-            arrDNS.get(i).setTime(Integer.parseInt(time));
+        System.out.println("0 - ввод с консоли");
+        System.out.println("1 - ввод с файла");
+        System.out.println("3 - вывод всей истории запросов");
+        int sw = input.nextInt();
+        switch (sw){
+            case(0):
+                System.out.println("Введите количество DNS-серверов");
+                n = input.nextInt();
+                for (int i = 0; i < n; i++){
+                    System.out.println("Введите адрес сервера DNS" + i +":");
+                    arrDNS.add(new DNS(input.next()));
+                    InetAddress inetAddress = java.net.InetAddress.getByName(arrDNS.get(i).getIp_addr());
+                    arrDNS.get(i).setName(inetAddress.getHostName());
+                    ProcessBuilder builder = new ProcessBuilder(
+                            "cmd.exe", "/c", "ping", arrDNS.get(i).getIp_addr());
+                    builder.redirectErrorStream(true);
+                    Process p = builder.start();
+                    BufferedReader r = new BufferedReader(new
+                            InputStreamReader(p.getInputStream(),"866"));
+                    String line;
+                    String fullline = "";
+                    while (true) {
+                        line = r.readLine();
+                        fullline=fullline+line;
+                        if (line == null) { break; }
+                    }
+                    index = fullline.indexOf("Среднее");
+                    if(index<0){
+                        System.out.println("DNS не доступен");
+                        arrDNS.get(i).setTime(-1);
+                        continue;
+                    }
+                    String time = fullline.substring(index+10, index+14);
+                    time = time.replaceAll("[a-zA-Zа-яА-Я]*", "");
+                    time = time.replaceAll(" ", "");
+                    arrDNS.get(i).setTime(Integer.parseInt(time));
+                }
+                Collections.sort(arrDNS, DNS.COMPARE_BY_TIME);
+                DNS.write(arrDNS);
+                System.out.println("Результат работы программы записан в директории /output");
+                break;
+            case(1):
+                System.out.println("Введите имя файла (он должен быть в текущей директории)");
+                String filename = input.next();
+                File doc = new File(filename);
+                Scanner obj = new Scanner(doc);
+                int i = 0;
+                while (obj.hasNextLine()){
+                    arrDNS.add(new DNS(obj.next()));
+                    InetAddress inetAddress = java.net.InetAddress.getByName(arrDNS.get(i).getIp_addr());
+                    arrDNS.get(i).setName(inetAddress.getHostName());
+                    ProcessBuilder builder = new ProcessBuilder(
+                            "cmd.exe", "/c", "ping", arrDNS.get(i).getIp_addr());
+                    builder.redirectErrorStream(true);
+                    Process p = builder.start();
+                    BufferedReader r = new BufferedReader(new
+                            InputStreamReader(p.getInputStream(),"866"));
+                    String line;
+                    String fullline = "";
+                    while (true) {
+                        line = r.readLine();
+                        fullline=fullline+line;
+                        if (line == null) { break; }
+                    }
+                    index = fullline.indexOf("Среднее");
+                    if(index<0){
+                        System.out.println("DNS не доступен");
+                        arrDNS.get(i).setTime(-1);
+                        continue;
+                    }
+                    String time = fullline.substring(index+10, index+14);
+                    time = time.replaceAll("[a-zA-Zа-яА-Я]*", "");
+                    time = time.replaceAll(" ", "");
+                    arrDNS.get(i).setTime(Integer.parseInt(time));
+                    i++;
+                }
+                Collections.sort(arrDNS, DNS.COMPARE_BY_TIME);
+                DNS.write(arrDNS);
+                System.out.println("Результат работы программы записан в директории /output");
+                break;
+            case(2):
+                break;
+            default:
+                break;
         }
-        Collections.sort(arrDNS, DNS.COMPARE_BY_TIME);
-        DNS.write(arrDNS);
-        System.out.println("Результат работы программы записан в директории /output");
+
     }
 }
-//77.88.8.7
-//1.1.1.1
-//8.8.8.8
-//87.240.137.158
