0dc893295ec3202c510d64251ab65847fff3adac
Rubanov Vasiliy <rubanovas2005@gmail.com>
2022-04-16 23:49:00 +0700

    add ask count of dns tests

--- lab2/src/rubanov/lab2.java
+++ lab2/src/rubanov/lab2.java
@@ -1,12 +1,16 @@
 package lab2.src.rubanov;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class lab2 {
 
     public static void main(String[] args) throws Exception {
         try {
-            new DNSAnalyzer().startTest(5).printTopTimes();
+            var scanner = new Scanner(System.in);
+            System.out.println("Enter the number of DNS servers:");
+            var testsCount = scanner.nextInt();
+            new DNSAnalyzer().startTest(testsCount).printTopTimes();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
