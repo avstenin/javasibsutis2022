ada05033591caf135948308e305c2f0a9bcc00da
Belreau Cohen <nuu.chah.nulth@disroot.org>
2022-05-03 10:35:41 +0700

    Определены датаклассы тарифов: YAML + внутренний

---
+++ lab4/src/kovalev/java/telecom/Tariff.java
@@ -0,0 +1,28 @@
+package telecom;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Tariff {
+    private String operatorName;
+    private String tariffName;
+    private Integer monthRub;
+    private Integer internetGB;
+    private Integer callMinutes;
+    private Integer sms;
+
+    @Override public String toString() {
+        return String.format("Имя оператора: %s\n", operatorName) +
+               String.format("Название тарифа: '%s'\n", tariffName) +
+               String.format("Стоимость: %d руб./мес.\n", monthRub) +
+               String.format("- Интернет (в ГБ): %d ГБ\n", internetGB) +
+               String.format("- Звонки: %d мин.\n", callMinutes) +
+               String.format("- СМС: %d сообщений\n", sms);
+    }
+}
---
+++ lab4/src/kovalev/java/telecom/YAMLOperator.java
@@ -0,0 +1,41 @@
+package telecom;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+class YAMLTariff {
+    private String tariffName;
+    private Integer monthRub;
+    private Integer internetGB;
+    private Integer callMinutes;
+    private Integer sms;
+}
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class YAMLOperator {
+    private String operatorName;
+    private List<YAMLTariff> tariffs;
+
+    public Tariff[] toTariffs() 
+    {
+        return tariffs.stream()
+                      .map(tariff -> new Tariff(operatorName,
+                                                tariff.getTariffName(),
+                                                tariff.getMonthRub(),
+                                                tariff.getInternetGB(),
+                                                tariff.getCallMinutes(),
+                                                tariff.getSms()))
+                      .toArray(Tariff[]::new);
+    }
+}
